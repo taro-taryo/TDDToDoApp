bc90294623b5c2c7ee5a47b7ed9a3432bef45892
taro-taryo <info.takanasystem@gmail.com>
2024-11-23 19:23:44 +0900

    XCode でプロジェクト新規作成

---
+++ TDDToDoApp/TDDToDoAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  TDDToDoAppApp.swift
+//  TDDToDoApp
+//  
+//  Created by taro-taryo on 2024/11/23
+//  
+//
+
+import SwiftUI
+
+@main
+struct TDDToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TDDToDoAppTests/TDDToDoAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  TDDToDoAppTests.swift
+//  TDDToDoAppTests
+//  
+//  Created by taro-taryo on 2024/11/23
+//  
+//
+
+import Testing
+@testable import TDDToDoApp
+
+struct TDDToDoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
